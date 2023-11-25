48d7d8ca7c086dc40a8916835cbc530b5be86c8d
Pasha-kto003 <lukovnikov.pasha@gmail.com>
2023-11-25 11:36:19 +1000

    ++update in Test2Window++

--- view/Test2Window.py
+++ view/Test2Window.py
@@ -1,6 +1,6 @@
 import csv
 import sys
-
+import numpy as np
 import torch
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QTableView, QPushButton, QHBoxLayout, QFileDialog, \
     QLabel
@@ -10,14 +10,19 @@ import os
 import cv2
 from sklearn.cluster import KMeans
 
-def extract_colors(image_path, num_colors=3):
+
+def extract_colors(image_path, num_colors=1):
     image = cv2.imread(image_path)
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+    results = model(image)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     pixels = image_rgb.reshape((-1, 3))
     kmeans = KMeans(n_clusters=num_colors)
     kmeans.fit(pixels)
     dominant_colors = kmeans.cluster_centers_.astype(int)
+    results.show()
     return dominant_colors
+
 
 class MyApplication(QWidget):
     def __init__(self):
@@ -77,58 +82,40 @@ class MyApplication(QWidget):
             self.result_label.setText("Выберите строку в таблице")
 
     def init_ui(self):
-        # Создаем модель данных для QTableView
         self.model = QStandardItemModel(0, 3)
         self.model.setHorizontalHeaderLabels(["Название", "Разрешение", "Вес"])
-
         self.result_label = QLabel("Выбранная запись:")
-        # Создаем QTableView
-
         self.table_view = QTableView()
         self.table_view.setModel(self.model)
-
-        # Создаем три кнопки
         btn_view_result = QPushButton('Просмотреть результат', self)
         btn_detection = QPushButton('Детекция', self)
         btn_exit = QPushButton('Выход', self)
         detect_button = QPushButton("Detect")
         find_button = QPushButton("Find Car")
-
-        # Подключаем обработчики событий для кнопок
         btn_view_result.clicked.connect(self.view_result)
         btn_detection.clicked.connect(self.detect)
         btn_exit.clicked.connect(self.exit_app)
         detect_button.clicked.connect(self.detectButtonClicked)
         find_button.clicked.connect(self.findcar_onimage)
-
-        # Создаем горизонтальный макет для кнопок
         button_layout = QHBoxLayout()
         button_layout.addWidget(btn_view_result)
         button_layout.addWidget(btn_detection)
         button_layout.addWidget(btn_exit)
         button_layout.addWidget(detect_button)
         button_layout.addWidget(find_button)
-
-        # Создаем вертикальный макет для основного окна
         main_layout = QVBoxLayout()
         main_layout.addWidget(self.table_view)
         main_layout.addLayout(button_layout)
         main_layout.addWidget(self.result_label)
-        # Устанавливаем макет в основное окно
         self.setLayout(main_layout)
-
-        # Устанавливаем размеры окна
         self.setFixedSize(1200, 700)
         self.setWindowTitle('Приложение с QTableView')
-
-        # Отображаем окно
         self.show()
 
     def view_result(self):
         folder_path = QFileDialog.getExistingDirectory(self, 'Выберите папку с изображениями')
         if folder_path:
             self.find_car(folder_path)
-
 
     def update_table(self, file_name, width, height, size_in_mbytes):
         # Добавляем данные в таблицу
@@ -139,7 +126,35 @@ class MyApplication(QWidget):
         self.model.setItem(row_position, 2, QStandardItem(f"{round(size_in_mbytes, 1)} Mbytes"))
 
     def detect(self):
-        print('Кнопка "Детекция" нажата!')
+        selected_index = self.table_view.selectionModel().currentIndex()
+        if selected_index.isValid():
+            selected_data = selected_index.siblingAtColumn(0).data()
+            image_path = rf'C:\Users\79249\PycharmProjects\pythonProject2\view\Datasets\images\{selected_data}'
+            img = cv2.imread(image_path)
+            color_bar, color = self.calc_metric(img, 100, 600, 900, 250)
+            cv2.imshow('Color bar', color_bar)
+            cv2.waitKey(0)
+
+    def create_bar(self, height, width, color):
+        bar = np.zeros((height, width, 3), np.uint8)
+        bar[:] = color
+        red, green, blue = int(color[2]), int(color[1]), int(color[0])
+
+        return bar, (red, green, blue)
+
+    def calc_metric(self, image, x, y, w, h):
+        # image = image[(y - h):y, x:(x + w)]
+        cv2.imshow('Image', image)
+        data = np.float32(image).reshape((-1, 3))
+
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        ret, label, center = cv2.kmeans(data, 2, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+        percentages = (np.unique(label, return_counts=True)[1]) / data.shape[0]
+
+        bar, color = self.create_bar(400, 400, center[np.argmax(percentages)])
+        img_bar = np.hstack((bar,))
+
+        return img_bar, color
 
     def exit_app(self):
         sys.exit()
